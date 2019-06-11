371c81c7b75fa0b5e56398ab8d2ebaed077a9020
Robert Góral <robertgoral@me.com>
2019-06-11 20:10:00 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+//console.log('siema!');
+const name='Robert';
+const js='konsoli';
+//console.log(name);
+//console.log(ko);
+//console.log(`${name}
+//${age}`);
+console.log(`Sie ma jestem ${name} witam
+w ${js} Powodzenia 👍`);
